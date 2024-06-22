aff862d4ed0266c212ff47f7a2bcf90382fff5f2
VijayaRatnaKumari <78976740+vijayaratnakumari@users.noreply.github.com>
2024-06-22 12:50:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -101,7 +101,7 @@ router.get("/create-and-save-person", function (req, res, next) {
         return next(err);
       }
       res.json(pers);
-      pers.remove();
+      pers.remove(); //if I hide this line then the person is saved in the MongoDB.
     });
   });
 });
