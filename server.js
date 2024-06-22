0676d43c1f5068d1c8b0b983f29968da3e4137f1
VijayaRatnaKumari <78976740+vijayaratnakumari@users.noreply.github.com>
2024-06-22 12:50:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -155,7 +155,7 @@ router.post("/find-all-by-name", function (req, res, next) {
         return next({ message: "Missing callback argument" });
       }
       res.json(data);
-      Person.remove().exec(); //if I hide this line then the person is saved in the MongoDB.
+      Person.remove().exec();
     });
   });
 });
